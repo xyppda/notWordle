b02be548479f2d979ad1e1bc25237f61d2419d89
vazheninEgor <m2310263@edu.misis.ru>
2025-05-05 22:16:53 +0300

    almost finished keyboard

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-import { WORDS } from "./words.js"
+import { WORDS } from "./words.js";
 import createKeyboard from "./createKeyboard.js";
 import createGameboard from "./createGameboard.js";
 
-createGameboard(5);
+const appState = {
+  lastFullLetterIndex: -1,
+  currentAttemptIndex: 0,
+};
+
+let wordLength = 5;
+
+createGameboard(wordLength);
 
 createKeyboard();
 
@@ -10,42 +17,67 @@ const langButton = document.getElementById("lang-button");
 const settingsButton = document.getElementById("settings-button");
 const helpButton = document.getElementById("help-button");
 
-langButton.addEventListener("click", () => {alert("В данный момент доступен только русский язык.")});
-
-settingsButton.addEventListener("click", () => {alert("Настройки будут добавлены позже.")});
-
-helpButton.addEventListener("click", () => {alert("Правила игры будут добавлены позже.")});
-
-const attempts = document.querySelectorAll("word-attempt");
-
-// TODO сделать словарь вида {номер попытки: все буквы текущей попытки (5 элементов)}
-// TOOD придумать как реализовать проход по объекту: цикл? просто переменная?
-// const wordsMap = new Map()
-
-const keys = document.querySelectorAll('.key');
-const letters = document.querySelectorAll('.letter');
-
-
-keys.forEach(key => {
-  key.addEventListener("click", () => {
-    const firstEmptyLetter = Array.from(letters).find(
-      container => container.textContent.trim() === ""
-    );
-    
-    if (firstEmptyLetter) {
-      firstEmptyLetter.textContent = key.textContent;
-    } else {
-      console.log("Все контейнеры уже заполнены!");
-    }
-  });
+langButton.addEventListener("click", () => {
+  alert("В данный момент доступен только русский язык.");
 });
 
-// TODO Доделать реализацию backspace
-// const backspace = document.querySelector("backspace-button");
+settingsButton.addEventListener("click", () => {
+  alert("Настройки будут добавлены позже.");
+});
 
-// backspace.addEventListener("click", () => {
+helpButton.addEventListener("click", () => {
+  alert("Правила игры будут добавлены позже.");
+});
 
-// })
+const setupKeyboardInput = (attempts) => {
+  const keys = document.querySelectorAll(".key");
+  const backspace = document.querySelector(".backspace-button");
+  const enterButton = document.querySelector(".enter-button");
 
-console.log(keys);
+  keys.forEach((key) => {
+    key.addEventListener("click", () => {
+      const attempt = [...attempts[appState.currentAttemptIndex].children];
+      const firstEmptyLetterIndex = attempt.findIndex(
+        (container) => container.textContent.trim() === ""
+      );
 
+      if (firstEmptyLetterIndex !== -1) {
+        attempt[firstEmptyLetterIndex].textContent = key.textContent;
+        appState.lastFullLetterIndex = firstEmptyLetterIndex;
+      } else {
+        console.log("Все контейнеры уже заполнены!");
+      }
+    });
+  });
+
+  backspace.addEventListener("click", () => {
+    const attempt = [...attempts[appState.currentAttemptIndex].children];
+    const lastFullLetterIndex = attempt.findLastIndex(
+      (container) => container.textContent !== ""
+    );
+    if (lastFullLetterIndex !== -1) {
+      attempt[lastFullLetterIndex].textContent = "";
+      appState.lastFullLetterIndex -= 1;
+    } else {
+      console.log("Все контейнеры пусты!");
+    }
+  });
+
+  enterButton.addEventListener("click", () => {
+    if (appState.lastFullLetterIndex === wordLength - 1) {
+      appState.currentAttemptIndex++;
+      appState.lastFullLetterIndex = -1;
+
+      if (appState.currentAttemptIndex < attempts.length) {
+        alert("Переход к следующей попытке...");
+      } else {
+        alert("Попытки закончились!");
+      }
+    } else {
+      alert("Заполните слово, перед переходом к следующей попытке.");
+    }
+  });
+};
+
+const attempts = document.querySelectorAll(".word-attempt");
+setupKeyboardInput(attempts);
