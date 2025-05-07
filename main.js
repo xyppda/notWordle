b9abf1f7845465876056ed5cbcefb2ab07cdf559
vazheninEgor <m2310263@edu.misis.ru>
2025-05-07 04:19:33 +0300

    almost done

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 import { WORDS } from "./words.js";
 import createKeyboard from "./createKeyboard.js";
 import createGameboard from "./createGameboard.js";
+import getRandomInt from "./getRandomInt.js";
+
+// TODO добавить выбор длины слов
+
+let wordLength = 5;
 
 const appState = {
   lastFullLetterIndex: -1,
   currentAttemptIndex: 0,
+  hiddenWord: "",
+  inputAvailability: false,
+  solve: Array(wordLength).fill(false),
 };
-
-let wordLength = 5;
-
-createGameboard(wordLength);
-
-createKeyboard();
 
 const langButton = document.getElementById("lang-button");
 const settingsButton = document.getElementById("settings-button");
@@ -29,77 +31,168 @@ helpButton.addEventListener("click", () => {
   alert("Правила игры будут добавлены позже.");
 });
 
+// Обработка ввода.
+
 const setupKeyboardInput = (attempts) => {
-  const keys = document.querySelectorAll(".key");
-  const backspace = document.querySelector(".backspace-button");
-  const enterButton = document.querySelector(".enter-button");
+  if (appState.inputAvailability) {
+    const keys = document.querySelectorAll(".key");
+    const backspace = document.querySelector(".backspace-button");
+    const enterButton = document.querySelector(".enter-button");
 
-  keys.forEach((key) => {
-    key.addEventListener("click", () => {
+    keys.forEach((key) => {
+      key.addEventListener("click", () => {
+        const attempt = [...attempts[appState.currentAttemptIndex].children];
+        const firstEmptyLetterIndex = attempt.findIndex(
+          (container) => container.textContent.trim() === ""
+        );
+
+        if (firstEmptyLetterIndex !== -1) {
+          const target = attempt[firstEmptyLetterIndex];
+          target.textContent = key.textContent;
+          appState.lastFullLetterIndex = firstEmptyLetterIndex;
+
+          target.classList.remove("letter-animated");
+          void target.offsetWidth;
+          target.classList.add("letter-animated");
+          target.classList.add("letter-complete");
+        }
+      });
+    });
+
+    backspace.addEventListener("click", () => {
       const attempt = [...attempts[appState.currentAttemptIndex].children];
-      const firstEmptyLetterIndex = attempt.findIndex(
-        (container) => container.textContent.trim() === ""
+      const lastFullLetterIndex = attempt.findLastIndex(
+        (container) => container.textContent !== ""
       );
-
-      if (firstEmptyLetterIndex !== -1) {
-        const target = attempt[firstEmptyLetterIndex];
-        target.textContent = key.textContent;
-        appState.lastFullLetterIndex = firstEmptyLetterIndex;
-
-        target.classList.remove("letter-animated");
-        void target.offsetWidth;
-        target.classList.add("letter-animated");
-        target.classList.add("letter-complete");
+      if (lastFullLetterIndex !== -1) {
+        const target = attempt[lastFullLetterIndex];
+        target.textContent = "";
+        target.classList.remove("letter-complete");
+        appState.lastFullLetterIndex -= 1;
       } else {
-        console.log("Все контейнеры уже заполнены!");
+        console.log("Все контейнеры пусты!");
       }
     });
-  });
 
-  backspace.addEventListener("click", () => {
-    const attempt = [...attempts[appState.currentAttemptIndex].children];
-    const lastFullLetterIndex = attempt.findLastIndex(
-      (container) => container.textContent !== ""
-    );
-    if (lastFullLetterIndex !== -1) {
-      const target = attempt[lastFullLetterIndex];
-      target.textContent = "";
-      target.classList.remove("letter-complete");
-      appState.lastFullLetterIndex -= 1;
-    } else {
-      console.log("Все контейнеры пусты!");
-    }
-  });
-
-  enterButton.addEventListener("click", () => {
-    if (appState.lastFullLetterIndex === wordLength - 1) {
-      appState.currentAttemptIndex++;
-      appState.lastFullLetterIndex = -1;
-
-      if (appState.currentAttemptIndex < attempts.length) {
-        alert("Переход к следующей попытке...");
-      } else {
-        alert("Попытки закончились!");
+    enterButton.addEventListener("click", () => {
+      if (appState.lastFullLetterIndex === wordLength - 1) {
+        if (appState.currentAttemptIndex < attempts.length - 1) {
+          const word = [...attempts[appState.currentAttemptIndex].children];
+          checkWord(word);
+          if (appState.solve.every(Boolean)) {
+            win(attempts);
+          } else {
+            appState.currentAttemptIndex++;
+            appState.lastFullLetterIndex = -1;
+          }
+        } else {
+          gameOver();
+        }
       }
-    } else {
-      alert("Заполните слово, перед переходом к следующей попытке.");
-    }
+    });
+  }
+};
+
+// Рендер кнопки "новая игра"
+
+const createNewGameButton = () => {
+  const placeholder = document.querySelector(".new-game-placeholder");
+  const button = document.createElement("button");
+  button.className = "new-game-button";
+  button.textContent = "Новая игра";
+  placeholder.append(button);
+  button.addEventListener("click", () => {
+    newGame();
   });
 };
 
-const attempts = document.querySelectorAll(".word-attempt");
-setupKeyboardInput(attempts);
+// Запуск игры.
+
+const cleareGame = () => {
+  document.querySelector(".game-board").innerHTML = "";
+  document.querySelector(".keyboard").innerHTML = "";
+  document.querySelector(".new-game-placeholder").innerHTML = "";
+};
+
+const newGame = () => {
+  appState.hiddenWord = WORDS[getRandomInt(0, WORDS.length)];
+  console.log(appState.hiddenWord);
+  cleareGame();
+  createGameboard(wordLength);
+  createKeyboard();
+  appState.inputAvailability = true;
+  appState.currentAttemptIndex = 0;
+  const attempts = document.querySelectorAll(".word-attempt");
+  setupKeyboardInput(attempts);
+};
+
+newGame();
+
+// Поражение в игре.
+
+const gameOver = () => {
+  if (
+    confirm(
+      `Вы проиграли. Загаданное слово: ${appState.hiddenWord}. Попробовать еще раз?`
+    )
+  ) {
+    newGame();
+  } else {
+    createNewGameButton();
+    appState.inputAvailability = false;
+  }
+};
+
+const win = (attempts) => {
+  const attemptIndex = appState.currentAttemptIndex + 1;
+  if (attemptIndex >= 5) {
+    alert(`Ура! Вы смогли угадать слово за ${attemptIndex} попыток!`);
+  } else if (attemptIndex >= 2) {
+    alert(`Ура! Вы смогли угадать слово за ${attemptIndex} попытки!`);
+  } else {
+    alert("Удача на Вашей стороне, Вы смогли угадать слово c первой попытки!");
+  }
+  for (let i = attemptIndex; i < attempts.length; i++) {
+    const target = [...attempts[i].children];
+    console.log(target);
+    target.forEach((letter) => {
+      letter.classList.add("letter-muted");
+    });
+  }
+  createNewGameButton();
+  appState.inputAvailability = false;
+};
+
+// TODO Проверка слова. Вызывается по нажатию Enter
+
+const checkWord = (word) => {
+  for (let i = 0; i < wordLength; i++) {
+    const letter = word[i];
+    const letterText = letter.textContent;
+    letter.classList.remove("letter-complete");
+    if (letterText === appState.hiddenWord[i]) {
+      letter.classList.add("letter-right");
+      appState.solve[i] = true;
+    } else if (appState.hiddenWord.includes(letterText)) {
+      letter.classList.add("letter-semi-right");
+      appState.solve[i] = false;
+    } else {
+      letter.classList.add("letter-wrong");
+      appState.solve[i] = false;
+    }
+  }
+};
+
+// Эмуляция ввода.
 
 document.addEventListener("keydown", (event) => {
   const key = event.key.toLowerCase();
   const virtualKeys = document.querySelectorAll(".key");
 
-  // Поиск обычной буквы
   const letterButton = Array.from(virtualKeys).find(
     (btn) => btn.textContent.toLowerCase() === key
   );
 
-  // ENTER
   if (key === "enter") {
     const enterButton = document.querySelector(".enter-button");
     if (enterButton) {
@@ -107,20 +200,14 @@ document.addEventListener("keydown", (event) => {
       enterButton.click();
       setTimeout(() => enterButton.classList.remove("active"), 100);
     }
-  }
-
-  // BACKSPACE
-  else if (key === "backspace") {
+  } else if (key === "backspace") {
     const backspaceButton = document.querySelector(".backspace-button");
     if (backspaceButton) {
       backspaceButton.classList.add("active");
       backspaceButton.click();
       setTimeout(() => backspaceButton.classList.remove("active"), 100);
     }
-  }
-
-  // Буквенная клавиша
-  else if (letterButton) {
+  } else if (letterButton) {
     letterButton.classList.add("active");
     letterButton.click();
     setTimeout(() => letterButton.classList.remove("active"), 100);
